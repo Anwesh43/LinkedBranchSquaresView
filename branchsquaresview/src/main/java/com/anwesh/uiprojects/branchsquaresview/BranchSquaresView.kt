bc622bd557778662efb09643e3a4d8248a2dd928
Anwesh43 <anweshthecool0@gmail.com>
2019-03-10 17:44:54 +0530

    adding BSNode class

--- branchsquaresview/src/main/java/com/anwesh/uiprojects/branchsquaresview/BranchSquaresView.kt
+++ branchsquaresview/src/main/java/com/anwesh/uiprojects/branchsquaresview/BranchSquaresView.kt
@@ -125,4 +125,48 @@ class BranchSquaresView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSNode(var i : Int, val state : State = State()) {
+
+        private var next : BSNode? = null
+        private var prev : BSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BSNode {
+            var curr : BSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
