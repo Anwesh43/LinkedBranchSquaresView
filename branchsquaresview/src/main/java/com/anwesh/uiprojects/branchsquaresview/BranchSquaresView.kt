e7f4ce10ebfa11937a72c682d94d101f23aa9224
Anwesh43 <anweshthecool0@gmail.com>
2019-03-10 17:07:45 +0530

    adding drawBSNode canvas extension method

--- branchsquaresview/src/main/java/com/anwesh/uiprojects/branchsquaresview/BranchSquaresView.kt
+++ branchsquaresview/src/main/java/com/anwesh/uiprojects/branchsquaresview/BranchSquaresView.kt
@@ -21,6 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rSizeFactor : Int = 3
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -28,3 +29,34 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawBSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val xGap : Float = size / (rects + 1)
+    val rSize : Float = xGap / rSizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawLine(-size, 0f, size, 0f, paint)
+    rotate(90f * sc2)
+    for (j in (0..(rects - 1))) {
+        val sc : Float = sc1.divideScale(j, rects)
+        val rectSize : Float = rSize * 0.5f * sc
+        save()
+        translate(-size + xGap * j, 0f)
+        drawLine(0f, 0f, 0f, size * sc, paint)
+        save()
+        translate(0f, size)
+        drawRect(RectF(-rectSize, -rectSize, rectSize, rectSize), paint)
+        restore()
+        restore()
+    }
+    restore()
+}
