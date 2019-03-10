58b08113f5926c1c33617197163f380be9ea25c8
Anwesh43 <anweshthecool0@gmail.com>
2019-03-10 17:58:51 +0530

    adding View  in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbranchsquaresview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbranchsquaresview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbranchsquaresview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.branchsquaresview.BranchSquaresView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BranchSquaresView.create(this)
     }
 }
